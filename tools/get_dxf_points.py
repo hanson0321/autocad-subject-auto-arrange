51acd2a4ed9de9b7bd81d11280de2c95da0435d8
luhluhluh <68135768+luhluhluh@users.noreply.github.com>
2024-07-04 17:36:21 +0800

    Update get_dxf_points.py

--- tools/get_dxf_points.py
+++ tools/get_dxf_points.py
@@ -1,16 +1,18 @@
-
 import ezdxf
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def get_points(file_path):
-    print("Getting points from dxf")
-    doc = ezdxf.readfile(file_path)
-    msp = doc.modelspace()
-    points = []
-    for e in msp:
-        if  e.dxftype() == "LWPOLYLINE":
-            df = pd.DataFrame(e.get_points('xy'))
-            points.append(e.get_points('xy'))
-    return points
+doc = ezdxf.readfile('/Users/lilianliao/Documents/研究所/Lab/Layout Generation/data/Test_Before.dxf')
+msp = doc.modelspace()
+points = []
+for e in msp:
+    if  e.dxftype() == "LWPOLYLINE":
+        df = pd.DataFrame(e.get_points('xy'))
+        points.append(e.get_points('xy'))
+    elif e.dxftype() == 'LINE':
+        start = e.dxf.start
+        end = e.dxf.end
+        points.append((start.x, start.y))
+        points.append((end.x, end.y))
+print(points)
