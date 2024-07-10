d796e55578935f93d17d4e6cc998e10c2c8f1e1f
clliu168 <clliu@nycu.edu.tw>
2024-07-10 10:51:19 +0800

    change variable names and provide comments for non-intersecting constraints.

--- layout_latestVer.py
+++ layout_latestVer.py
@@ -39,14 +39,14 @@ unusable_gridcell = {
     0:{'x':0,'y':0,'w':100, 'h':30},
     1:{'x':500,'y':0,'w':150, 'h':50}
 }
-n = len(obj_params)
-m = len(unusable_gridcell)
+num_objects = len(obj_params)
+num_unusable_cells = len(unusable_gridcell)
 
 
 # Space parameters
-W = 1700
-H = 1100
-A = 100
+SPACE_WIDTH = 1700
+SPACE_HEIGHT = 1100
+AISLE_SPACE = 100
 
 # Fixed object position
 fixed_room = 0
@@ -55,7 +55,7 @@ y_fixed = 0
 
 
 
-def layout_opt(obj_params, n, A, W, H, unusable_gridcell, m):
+def layout_opt(obj_params, num_objects, AISLE_SPACE, SPACE_WIDTH, SPACE_HEIGHT, unusable_gridcell, num_unusable_cells):
     # Create a Gurobi model
     start_time = time.time()
     model = gp.Model("layout_generation_front desk")
@@ -64,95 +64,95 @@ def layout_opt(obj_params, n, A, W, H, unusable_gridcell, m):
 
     # Binary variables
     p = {}
-    for i in range(n):
-        for j in range(n):
+    for i in range(num_objects):
+        for j in range(num_objects):
             if i != j:
                 p[i, j] = model.addVar(vtype=GRB.BINARY, name=f"p_{i}_{j}")
     q = {}
-    for i in range(n):
-        for j in range(n):
+    for i in range(num_objects):
+        for j in range(num_objects):
             if i != j:
                 q[i, j] = model.addVar(vtype=GRB.BINARY, name=f"q_{i}_{j}")
 
     s = {}
-    for i in range(n):
-        for k in range(m):
+    for i in range(num_objects):
+        for k in range(num_unusable_cells):
             s[i, k] = model.addVar(vtype=GRB.BINARY, name=f"s_{i}_{k}")
     t = {}
-    for i in range(n):
-        for k in range(m):
+    for i in range(num_objects):
+        for k in range(num_unusable_cells):
             t[i, k] = model.addVar(vtype=GRB.BINARY, name=f"t_{i}_{k}")
 
     orientation = {}
-    for i in range(n):
+    for i in range(num_objects):
         orientation[i] = model.addVar(vtype=GRB.BINARY, name=f"orientation_{i}")
     # Dimension variables
     x = {}
-    for k in range(n):
+    for k in range(num_objects):
         x[k] = model.addVar(vtype=GRB.CONTINUOUS, name=f"x_{k}")
     
     y = {}
-    for k in range(n):
+    for k in range(num_objects):
         y[k] = model.addVar(vtype=GRB.CONTINUOUS, name=f"y_{k}")
     w = {}
-    for k in range(n):
+    for k in range(num_objects):
         w[k] = model.addVar(vtype=GRB.CONTINUOUS, name=f"w_{k}")
     h = {}
-    for k in range(n):
+    for k in range(num_objects):
         h[k] = model.addVar(vtype=GRB.CONTINUOUS, name=f"h_{k}")
 
     select = {}
-    for i in range(n):
+    for i in range(num_objects):
         for k in range(4):
             select[i,k] = model.addVar(vtype=GRB.BINARY, name=f"select_{i,k}")
 
     select2 = {}
-    for i in range(n):
+    for i in range(num_objects):
         for k in range(4):
             select2[i,k] = model.addVar(vtype=GRB.BINARY, name=f"select2_{i,k}")
     
     T = {}
-    for i in range(n):
-        for k in range(n):
+    for i in range(num_objects):
+        for k in range(num_objects):
             T[i,k] = model.addVar(vtype=GRB.CONTINUOUS, name=f"T_{i,k}")
 
 
     # Set objective
     #model.setObjective(gp.quicksum(w[i]*h[i] for i in range(n)), GRB.MINIMIZE)
     #model.setParam('TimeLimit', 1800)
-    total_area = gp.quicksum(w[i] * h[i] for i in range(n))
-    coor =gp.quicksum(x[i] + y[i] for i in range(n))
+    total_area = gp.quicksum(w[i] * h[i] for i in range(num_objects))
+    coor =gp.quicksum(x[i] + y[i] for i in range(num_objects))
     model.setObjective((total_area), GRB.MINIMIZE)
     # Set constraints for general specifications
     # Connectivity constraint
-    for i in range(n):
+    for i in range(num_objects):
         if not obj_params[i]['connect']:
             print(f'No connectivity constraint for object {i}')
         else:
             for j in obj_params[i]['connect']:
                 print(f'connect{j}')
-                model.addConstr(x[i] + w[i] >= x[j] - W*(p[i,j] + q[i,j]), name="Connectivity Constraint 1")
-                model.addConstr(y[i] + h[i] >= y[j] - H*(1 + p[i,j] - q[i,j]), name="Connectivity Constraint 2")
-                model.addConstr(x[j] + w[j] >= x[i] - W*(1 - p[i,j] + q[i,j]), name = "Connectivity Constraint 3")
-                model.addConstr(y[j] + h[j] >= y[i] - H*(2 - p[i,j] - q[i,j]), name = "Connectivity Constraint 4")
-                model.addConstr(0.5*(w[i]+w[j]) >= T[i,j] + (y[j] - y[i]) - W*(p[i,j] + q[i,j]), name="overlap constraint_18")
-                model.addConstr(0.5*(h[i]+h[j]) >= T[i,j] + (x[j] - x[i]) - H*(2 - p[i,j] - q[i,j]), name="overlap constraint_19")
-                model.addConstr(0.5*(w[i]+w[j]) >= T[i,j] + (y[i] - y[j]) - W*(1 - p[i,j] + q[i,j]), name="overlap constraint_20")
-                model.addConstr(0.5*(h[i]+h[j]) >= T[i,j] + (x[i] - x[j]) - H*(1 + p[i,j] - q[i,j]), name="overlap constraint_21")
+                model.addConstr(x[i] + w[i] >= x[j] - SPACE_WIDTH*(p[i,j] + q[i,j]), name="Connectivity Constraint 1")
+                model.addConstr(y[i] + h[i] >= y[j] - SPACE_HEIGHT*(1 + p[i,j] - q[i,j]), name="Connectivity Constraint 2")
+                model.addConstr(x[j] + w[j] >= x[i] - SPACE_WIDTH*(1 - p[i,j] + q[i,j]), name = "Connectivity Constraint 3")
+                model.addConstr(y[j] + h[j] >= y[i] - SPACE_HEIGHT*(2 - p[i,j] - q[i,j]), name = "Connectivity Constraint 4")
+                model.addConstr(0.5*(w[i]+w[j]) >= T[i,j] + (y[j] - y[i]) - SPACE_WIDTH*(p[i,j] + q[i,j]), name="overlap constraint_18")
+                model.addConstr(0.5*(h[i]+h[j]) >= T[i,j] + (x[j] - x[i]) - SPACE_HEIGHT*(2 - p[i,j] - q[i,j]), name="overlap constraint_19")
+                model.addConstr(0.5*(w[i]+w[j]) >= T[i,j] + (y[i] - y[j]) - SPACE_WIDTH*(1 - p[i,j] + q[i,j]), name="overlap constraint_20")
+                model.addConstr(0.5*(h[i]+h[j]) >= T[i,j] + (x[i] - x[j]) - SPACE_HEIGHT*(1 + p[i,j] - q[i,j]), name="overlap constraint_21")
 
     # Boundary constraints
-    for i in range(n):
-        model.addConstr(x[i] + w[i] <= W, name="Boundary constraint for x")
-        model.addConstr(y[i] + h[i] <= H, name="Boundary constraint for y")
+    for i in range(num_objects):
+        model.addConstr(x[i] + w[i] <= SPACE_WIDTH, name="Boundary constraint for x")
+        model.addConstr(y[i] + h[i] <= SPACE_HEIGHT, name="Boundary constraint for y")
     
     # Fixed border constraint
-    for i in range(n):
+    for i in range(num_objects):
         if obj_params[i]['fixed_wall'] == 'north':
             model.addConstr(y[i] == 0, name="North border constraint")
         elif obj_params[i]['fixed_wall']== 'south':
-            model.addConstr(y[i]+max(obj_params[i]['w_h']) == H, name="South border constraint")
+            model.addConstr(y[i]+max(obj_params[i]['w_h']) == SPACE_HEIGHT, name="South border constraint")
         elif obj_params[i]['fixed_wall']== 'east':
-            model.addConstr(x[i]+w[i] == W, name="East border constraint")
+            model.addConstr(x[i]+w[i] == SPACE_WIDTH, name="East border constraint")
         elif obj_params[i]['fixed_wall']== 'west':
             model.addConstr(x[i] == 0, name="West border constraint")
         
@@ -161,51 +161,59 @@ def layout_opt(obj_params, n, A, W, H, unusable_gridcell, m):
             model.addConstr(select[i,0] + select[i,1] + select[i,2] +select[i,3] == 1)
             # 限制長邊靠牆
             model.addConstr((select[i,0] + select[i,1])*(1-orientation[i]) + (select[i,2] +select[i,3])*orientation[i] == 1)
-            model.addConstr((x[i]+1)*select[i,0]+(x[i]+min(obj_params[i]['w_h'])-W+1)*select[i,1]+(y[i]+1)*select[i,2]+(y[i]+min(obj_params[i]['w_h'])-H+1)*select[i,3]==1, name='any constraint')
+            model.addConstr((x[i]+1)*select[i,0]+(x[i]+min(obj_params[i]['w_h'])-SPACE_WIDTH+1)*select[i,1]+(y[i]+1)*select[i,2]+(y[i]+min(obj_params[i]['w_h'])-SPACE_HEIGHT+1)*select[i,3]==1, name='any constraint')
         
         else:
             print(f'No fixed wall constraint for object {i}')
 
     
     # Non-intersecting with aisle constraint
-    for i in range(n):
-        for j in range(n):
+    for i in range(num_objects):
+        for j in range(num_objects):
             if not obj_params[i]['connect'] and i != j:
                 if [i, j] == [1,0] or [i,j]==[0,1]:
-                    model.addConstr((orientation[i]==0)>>(x[i] + w[i] + 110 <= x[j] + W * (p[i,j] + q[i,j])), name="Non-intersecting Constraint 1")
-                    model.addConstr((orientation[i]==1)>>(y[i] + h[i] + 110 <= y[j] + H * (1 + p[i,j] - q[i,j])), name="Non-intersecting Constraint 2")
-                    model.addConstr((orientation[i]==0)>>(x[j] + w[j] + 110 <= x[i] + W * (1 - p[i,j] + q[i,j])), name = "Non-intersecting Constraint 3")
-                    model.addConstr((orientation[i]==1)>>(y[j] + h[j] + 110 <= y[i] + H * (2 - p[i,j] - q[i,j])), name = "Non-intersecting Constraint 4")
+                    model.addConstr((orientation[i]==0)>>(x[i] + w[i] + 110 <= x[j] + SPACE_WIDTH * (p[i,j] + q[i,j])), name="Non-intersecting Constraint 1")
+                    model.addConstr((orientation[i]==1)>>(y[i] + h[i] + 110 <= y[j] + SPACE_HEIGHT * (1 + p[i,j] - q[i,j])), name="Non-intersecting Constraint 2")
+                    model.addConstr((orientation[i]==0)>>(x[j] + w[j] + 110 <= x[i] + SPACE_WIDTH * (1 - p[i,j] + q[i,j])), name = "Non-intersecting Constraint 3")
+                    model.addConstr((orientation[i]==1)>>(y[j] + h[j] + 110 <= y[i] + SPACE_HEIGHT * (2 - p[i,j] - q[i,j])), name = "Non-intersecting Constraint 4")
 
-                    model.addConstr((orientation[i]==0)>>(x[i] + w[i] + 110 >= x[j] - W * (p[i,j] + q[i,j])), name="Non-intersecting Constraint 1")
-                    model.addConstr((orientation[i]==1)>>(y[i] + h[i] + 110 >= y[j] - H * (1 + p[i,j] - q[i,j])), name="Non-intersecting Constraint 2")
-                    model.addConstr((orientation[i]==0)>>(x[j] + w[j] + 110 >= x[i] - W * (1 - p[i,j] + q[i,j])), name = "Non-intersecting Constraint 3")
-                    model.addConstr((orientation[i]==1)>>(y[j] + h[j] + 110 >= y[i] - H * (2 - p[i,j] - q[i,j])), name = "Non-intersecting Constraint 4")
+                    model.addConstr((orientation[i]==0)>>(x[i] + w[i] + 110 >= x[j] - SPACE_WIDTH * (p[i,j] + q[i,j])), name="Non-intersecting Constraint 1")
+                    model.addConstr((orientation[i]==1)>>(y[i] + h[i] + 110 >= y[j] - SPACE_HEIGHT * (1 + p[i,j] - q[i,j])), name="Non-intersecting Constraint 2")
+                    model.addConstr((orientation[i]==0)>>(x[j] + w[j] + 110 >= x[i] - SPACE_WIDTH * (1 - p[i,j] + q[i,j])), name = "Non-intersecting Constraint 3")
+                    model.addConstr((orientation[i]==1)>>(y[j] + h[j] + 110 >= y[i] - SPACE_HEIGHT * (2 - p[i,j] - q[i,j])), name = "Non-intersecting Constraint 4")
                 else:
-                    model.addConstr(x[i] + w[i] + A <= x[j] + W * (p[i,j] + q[i,j]), name="Non-intersecting Constraint 1")
-                    model.addConstr(y[i] + h[i] + A <= y[j] + H * (1 + p[i,j] - q[i,j]), name="Non-intersecting Constraint 2")
-                    model.addConstr(x[j] + w[j] + A <= x[i] + W * (1 - p[i,j] + q[i,j]), name = "Non-intersecting Constraint 3")
-                    model.addConstr(y[j] + h[j] + A <= y[i] + H * (2 - p[i,j] - q[i,j]), name = "Non-intersecting Constraint 4")
+                    # non-intersecting constraints
+                    # object i to the left of object j
+                    model.addConstr(x[i] + w[i] + AISLE_SPACE <= x[j] + SPACE_WIDTH * (p[i,j] + q[i,j]), name="Non-intersecting Constraint 1")
+                    
+                    # object i above object j
+                    model.addConstr(y[i] + h[i] + AISLE_SPACE <= y[j] + SPACE_HEIGHT * (1 + p[i,j] - q[i,j]), name="Non-intersecting Constraint 2")
+                    
+                    # object i to the right of object j
+                    model.addConstr(x[j] + w[j] + AISLE_SPACE <= x[i] + SPACE_WIDTH * (1 - p[i,j] + q[i,j]), name = "Non-intersecting Constraint 3")
+                    
+                    # object i below object j
+                    model.addConstr(y[j] + h[j] + AISLE_SPACE <= y[i] + SPACE_HEIGHT * (2 - p[i,j] - q[i,j]), name = "Non-intersecting Constraint 4")
             elif i!=j:
-                model.addConstr(x[i] + w[i] <= x[j] + W * (p[i,j] + q[i,j]), name="Non-intersecting Constraint 1")
-                model.addConstr(y[i] + h[i] <= y[j] + H * (1 + p[i,j] - q[i,j]), name="Non-intersecting Constraint 2")
-                model.addConstr(x[j] + w[j] <= x[i] + W * (1 - p[i,j] + q[i,j]), name = "Non-intersecting Constraint 3")
-                model.addConstr(y[j] + h[j] <= y[i] + H * (2 - p[i,j] - q[i,j]), name = "Non-intersecting Constraint 4")
+                model.addConstr(x[i] + w[i] <= x[j] + SPACE_WIDTH * (p[i,j] + q[i,j]), name="Non-intersecting Constraint 1")
+                model.addConstr(y[i] + h[i] <= y[j] + SPACE_HEIGHT * (1 + p[i,j] - q[i,j]), name="Non-intersecting Constraint 2")
+                model.addConstr(x[j] + w[j] <= x[i] + SPACE_WIDTH * (1 - p[i,j] + q[i,j]), name = "Non-intersecting Constraint 3")
+                model.addConstr(y[j] + h[j] <= y[i] + SPACE_HEIGHT * (2 - p[i,j] - q[i,j]), name = "Non-intersecting Constraint 4")
 
     # Length constraint
-    for i in range(n):
+    for i in range(num_objects):
         model.addConstr(w[i]==[min(obj_params[i]['w_h']),max(obj_params[i]['w_h'])] , name="Length Constraint 1")
         model.addConstr(h[i] == [min(obj_params[i]['w_h']), max(obj_params[i]['w_h'])], name="Height Constraint 2")
     
     # Unusable grid cell constraint
-    for i in range(n):
-        for k in range(m):
-            model.addConstr(x[i] >= unusable_gridcell[k]['x']+ unusable_gridcell[k]['w'] + 1 - W * (s[i,k] + t[i,k]), name="Unusable grid cell 1")
-            model.addConstr(unusable_gridcell[k]['x'] >= x[i] + w[i] - W * (1 + s[i,k] - t[i,k]), name="Unusable grid cell 2")
-            model.addConstr(y[i] >= unusable_gridcell[k]['y'] + unusable_gridcell[k]['h'] + 1 - H * (1 - s[i,k] + t[i,k]), name = "Unusable grid cell 3")
-            model.addConstr(unusable_gridcell[k]['y'] >= y[i] + h[i] - H * (2 - s[i,k] - t[i,k]), name = "Unusable grid cell 4")
+    for i in range(num_objects):
+        for k in range(num_unusable_cells):
+            model.addConstr(x[i] >= unusable_gridcell[k]['x']+ unusable_gridcell[k]['w'] + 1 - SPACE_WIDTH * (s[i,k] + t[i,k]), name="Unusable grid cell 1")
+            model.addConstr(unusable_gridcell[k]['x'] >= x[i] + w[i] - SPACE_WIDTH * (1 + s[i,k] - t[i,k]), name="Unusable grid cell 2")
+            model.addConstr(y[i] >= unusable_gridcell[k]['y'] + unusable_gridcell[k]['h'] + 1 - SPACE_HEIGHT * (1 - s[i,k] + t[i,k]), name = "Unusable grid cell 3")
+            model.addConstr(unusable_gridcell[k]['y'] >= y[i] + h[i] - SPACE_HEIGHT * (2 - s[i,k] - t[i,k]), name = "Unusable grid cell 4")
     # Orientation constraint
-    for i in range(n):
+    for i in range(num_objects):
         model.addConstr(w[i] == h[i]*((max(obj_params[i]['w_h'])/min(obj_params[i]['w_h']))*orientation[i]+(min(obj_params[i]['w_h'])/max(obj_params[i]['w_h']))*(1-orientation[i])))
     
     # Set constraints related to front desk
@@ -218,8 +226,8 @@ def layout_opt(obj_params, n, A, W, H, unusable_gridcell, m):
     model.addConstr((orientation[0]==0)>>(y[0]==y[1]))
 
     # Constraint for object long side againt wall
-    for i in range(n):
-        for j in range(n):
+    for i in range(num_objects):
+        for j in range(num_objects):
             if [i, j] == [1,0]:
                 model.addConstr((q[i,j]==1)>>(orientation[i]==1))
                 model.addConstr((q[i,j]==0)>>(orientation[i]==0))
@@ -234,7 +242,7 @@ def layout_opt(obj_params, n, A, W, H, unusable_gridcell, m):
     if model.status == GRB.OPTIMAL:
         print(f"Runtime: {end_time - start_time} seconds")
         print("Optimal solution found!")
-        for i in range(n):
+        for i in range(num_objects):
             print(f"Object {i}: x={x[i].X}, y={y[i].X}, w={w[i].X}, h={h[i].X}")
             result.update({i:{'x':x[i].X, 'y':y[i].X, 'w':w[i].X, 'h':h[i].X}})
         print("Total area:", model.objVal)
@@ -256,7 +264,7 @@ def layout_plot(result, unusable_gridcell):
     data = result
 
     # Define total space dimensions
-    total_space = {'width': W, 'height': H}
+    total_space = {'width': SPACE_WIDTH, 'height': SPACE_HEIGHT}
 
     # Create a new figure
     plt.figure(figsize=(8, 8))
@@ -319,7 +327,7 @@ def draw_dxf(result,name,W,H):
 
 
 if __name__ == '__main__':
-    result = layout_opt(obj_params, n, A,W, H, unusable_gridcell, m)
-    draw_dxf(result,"20240620_result",W,H)
+    result = layout_opt(obj_params, num_objects, AISLE_SPACE,SPACE_WIDTH, SPACE_HEIGHT, unusable_gridcell, num_unusable_cells)
+    draw_dxf(result,"20240620_result",SPACE_WIDTH,SPACE_HEIGHT)
     layout_plot(result, unusable_gridcell)
     
