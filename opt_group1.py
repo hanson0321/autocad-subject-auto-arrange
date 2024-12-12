a0f470e08f34f6e47130b5e988bcc02fbca12846
luhluhluh <68135768+luhluhluh@users.noreply.github.com>
2024-12-12 21:20:43 +0800

    Update opt_group1.py

--- opt_group1.py
+++ opt_group1.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from shapely.geometry import Polygon, box, LineString
+from shapely.geometry import Polygon, box, LineString, Point
 import time
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -25,6 +25,22 @@ def calculate_edge_lengths(edges):
     for edge in edges:
         lengths[edge] = edge.length
     return lengths
+
+def define_orientation(obj,poly_feasible):
+    ### 門前至少10cm
+    if(obj['w']>obj['h']): ### 橫著擺 面南北
+        point = Point(obj['x'] + obj['w']/2, obj['y']-10)
+        if(poly_feasible.contains(point)):
+            obj_orientation = 'north'
+        else:
+            obj_orientation = 'south'
+    else: ### 直著擺 面東西
+        point = Point(obj['x']-10, obj['y'] + obj['h']/2)
+        if(poly_feasible.contains(point)):
+            obj_orientation = 'east'
+        else:
+            obj_orientation = 'west'
+    return obj_orientation
 
 def place_object_along_wall(obj_params, edges, preplaced_polygons, room_polygon, space_min_x, space_min_y, space_max_x, space_max_y, door_opening_space, unusable_gridcell, unusable_gridcell0):
     rectangles= [params['w_h'] for params in obj_params.values() if params['group'] == 1]
@@ -142,11 +158,19 @@ def place_object_along_wall(obj_params, edges, preplaced_polygons, room_polygon,
         if not rectangles:
             print('All rectangles placed!')
             break
+    
     placement_with_door = create_dict_from_polygons(placement_with_door, obj_params)
     unusable_gridcell1 = {}
     values = list(unusable_gridcell.values()) + list(unusable_gridcell0.values())+list(placement_with_door.values())
     unusable_gridcell1 = {i: values[i] for i in range(len(values))}
     placements_dict = create_dict_from_polygons(placements, obj_params)
+    max_key = max(unusable_gridcell0.keys())
+    for key, values in placements_dict.items():
+        max_key += 1
+        placements_dict[key]['type'] = 'objects'
+        placements_dict[key]['orientation'] = define_orientation(values,room_polygon)
+        unusable_gridcell0[max_key] = values
+        
     return placements, placements_dict, unusable_gridcell1
 
 
@@ -209,7 +233,7 @@ if __name__ == '__main__':
     }
     
     # Create pre-placed polygons
-    counter_result, counter_placement, unusable_gridcell0, available_segments = opt_group0.counter_placements(poly_feasible, obj_params, COUNTER_SPACING, LINEUP_SPACING, DOOR_PLACEMENT, DOOR_ENTRY, min_x, max_x, min_y, max_y)
+    counter_result, counter_placement, unusable_gridcell0, available_segments = opt_group0.counter_placements(unusable_gridcell, poly_feasible, obj_params, COUNTER_SPACING, LINEUP_SPACING, DOOR_PLACEMENT, DOOR_ENTRY, min_x, max_x, min_y, max_y)
     preplaced_polygons = create_preplaced_polygons(unusable_gridcell0)
     placements_polygon, placements, unusable_gridcell1 = place_object_along_wall(obj_params, available_segments, preplaced_polygons,poly_feasible, min_x, min_y, max_x, max_y, OPENDOOR_SPACING, unusable_gridcell, unusable_gridcell0)
     # Convert list of polygons to dictionary
